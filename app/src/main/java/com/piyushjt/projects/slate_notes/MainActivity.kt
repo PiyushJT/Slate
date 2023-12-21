cfde97ca36b55d8902b3d4f1b32de73cdff6382f
Piyush_jt__ <piyushthummar396@gmail.com>
2023-12-21 14:09:54 +0530

    enhanced ux for creating account

--- app/src/main/java/com/piyushjt/projects/slate_notes/MainActivity.kt
+++ app/src/main/java/com/piyushjt/projects/slate_notes/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.piyushjt.projects.slate_notes
 
-import android.R
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,42 +9,58 @@ import com.piyushjt.projects.slate_notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     companion object {
         lateinit var auth: FirebaseAuth
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
-
         setContentView(binding.root)
 
-        binding.signUp.setOnClickListener {
+        visibilitySignUpSignOut()
 
+        // Going to register activity by signUp button
+        binding.signUp.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
 
+
+        // signing out the user
+        binding.signOut.setOnClickListener {
+            auth.signOut()
+            binding.userDetails.text = updateData()
+            visibilitySignUpSignOut()
         }
     }
-
 
     override fun onResume() {
         super.onResume()
         binding.userDetails.text = updateData()
-
-        if(binding.userDetails.text != "Not Signed In"){
-            binding.signUp.visibility= View.INVISIBLE
-        }
+        visibilitySignUpSignOut()
     }
 
+
+    // function to change name of user in main activity
     private fun updateData(): String {
         var email= "${auth.currentUser?.email}"
         email= email.substringBeforeLast("@")
         return email
+    }
+
+
+    // function to change visibility of signIn and signOut btns
+    private fun visibilitySignUpSignOut(){
+        if (binding.userDetails.text != "null") {
+            binding.signUp.visibility = View.INVISIBLE
+            binding.signOut.visibility = View.VISIBLE
+        }
+        else{
+            binding.signUp.visibility= View.VISIBLE
+            binding.signOut.visibility= View.INVISIBLE
+        }
     }
 
 }
--- app/src/main/java/com/piyushjt/projects/slate_notes/RegisterActivity.kt
+++ app/src/main/java/com/piyushjt/projects/slate_notes/RegisterActivity.kt
@@ -3,35 +3,52 @@ package com.piyushjt.projects.slate_notes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.animation.AnimationUtils
 import android.widget.Toast
 import com.piyushjt.projects.slate_notes.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
         binding = ActivityRegisterBinding.inflate(layoutInflater)
-
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
+        // Creating account
         binding.createAccountBtn.setOnClickListener {
+
+            // getting user details
             val email = binding.emailRegister.text.toString()
+            val username = binding.usernameRegister.text.toString()
             val password = binding.passwordRegister.text.toString()
 
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                MainActivity.auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            startActivity(Intent(this, LoginActivity::class.java))
-                            finish()
+            // checking for emptiness and nulls
+            if (email.isNotEmpty() && password.isNotEmpty() && username.isNotEmpty()) {
+                if (username != "null") {
+
+                    // account creation
+                    MainActivity.auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+
+                            // success
+                            if (it.isSuccessful) {
+                                startActivity(Intent(this, MainActivity::class.java))
+                                finish()
+                            }
+
+                            // failure report
+                        }.addOnFailureListener {
+                            Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
                         }
-                    }.addOnFailureListener {
-                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
                 }
+
+                // if username is conflicted with default null
+                else{Toast.makeText(this, "username cannot be null", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            // if all the details are not provided
+            else{
+                Toast.makeText(this, "Please fill all the details", Toast.LENGTH_LONG).show()
             }
         }
 
